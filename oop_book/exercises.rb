8173eb3e86083eed85c4c4aa2d67df9665911e24
Tom Lee <tom_t_lee@yahoo.com>
2015-02-12 15:44:50 +0000

    Finish OOP book

--- oop_book/exercises.rb
+++ oop_book/exercises.rb
@@ -1,9 +1,21 @@
-class MyCar
+module Towable
+  def can_tow?(pounds)
+    pounds < 2000 ? true : false
+  end
+end
+
+class Vehicle
+  @@number_of_vehicles = 0
+  
   attr_accessor :color
   attr_reader :year, :model
   
   def self.gas_mileage(gallons, miles)
     puts "#{miles / gallons} miles per gallon of gas."
+  end
+
+  def self.number_of_vehicles
+    @@number_of_vehicles
   end
   
   def initialize(year, color, model)
@@ -11,6 +23,7 @@ class MyCar
     @color = color
     @model = model
     @current_speed = 0
+    @@number_of_vehicles += 1
   end
   
   def speed_up(speed)
@@ -33,11 +46,42 @@ class MyCar
     puts "The car has been painted #{color}."
   end
   
+  def age
+    puts "This #{model} is #{years_old} years old."
+  end
+  
+  private
+    
+    def years_old
+      Time.now.year - year.to_i
+    end
+
+end
+
+
+class MyCar < Vehicle
+
+  NUMBER_OF_DOORS = 4
+  
   def to_s
     "My car is a #{color} #{year} #{model}."
   end
+end
+
+
+class MyTruck < Vehicle
+  include Towable
   
+  def to_s
+    "My truck is a #{color} #{year} #{model}."
+  end
 end
 
 my_car = MyCar.new("2010", "silver", "Ford Focus")
 puts my_car
+my_truck = MyTruck.new("1987", "Green", "Unimog")
+puts my_truck
+puts Vehicle.number_of_vehicles
+puts my_truck.can_tow?(3000)
+
+puts my_truck.age
