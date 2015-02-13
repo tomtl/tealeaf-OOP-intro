13375c3511a2a35a01606e9559812dc85ee09329
Tom Lee <tom_t_lee@yahoo.com>
2015-02-13 20:48:53 +0000

    Tic tac toe - start win check method

--- tic-tac-toe/tic-tac-toe.rb
+++ tic-tac-toe/tic-tac-toe.rb
@@ -2,6 +2,12 @@
 class Board
   attr_reader :positions
   
+  WINNING_COMBINATIONS =  [
+    [1, 4, 7], [2, 5, 8], [3, 6, 9],  # verticals
+    [1, 2, 3], [4, 5, 6], [7, 8, 9],  # horizontals
+    [1, 5, 9], [3, 5, 7]              # diagonals
+  ]
+  
   def initialize
     @positions = {1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5,
               6 => 6, 7 => 7, 8 => 8, 9 => 9}
@@ -18,7 +24,7 @@ class Board
   end
   
   def update(player, position)
-    x_or_o = {"user" => "X", "computer" => "o"}
+    
     self.positions[positions.key(position)] = x_or_o[player]
     positions
   end
@@ -36,9 +42,14 @@ class Board
   end
   
   def check_for_winner
-    # check if anyone won
-    # check if tie
-    # return winner (person/computer), tie or nil
+    player_positions = get_user_positions(board) if player == "user"
+    player_positions = get_computer_positions(board) if player == "computer"
+    winning_combinations.each do |combination|
+      if (combination & player_positions) == combination
+        return true
+      end
+    end
+    nil
   end
   
 end
@@ -59,17 +70,25 @@ class Person < Player
       user_pick = gets.chomp
     end
     return user_pick
-  end 
+  end
+  
+  def positions
+    player_positions = []
+    board.each { |position, value| player_positions << position if value == "X" }
+    user_positions
+  end
+  
 end
 
 class Computer < Player
   def pick
-    # pick a good position
+    board.available.sample
   end
 end
 
-class TicTacToe
+class Game
   attr_accessor :player, :computer, :board
+  
   def initialize
     @person = Person.new
     @computer = Computer.new
