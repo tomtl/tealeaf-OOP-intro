82cd46e57019e8d49c31c59f687e6cae1e5b3269
Tom Lee <tom_t_lee@yahoo.com>
2015-02-13 14:20:38 +0000

    Tic-tac-toe initial crack

--- tic-tac-toe/tic-tac-toe.rb
+++ tic-tac-toe/tic-tac-toe.rb
@@ -3,34 +3,80 @@
 # computer class
 
 class Board
-  attr_reader :board
+  attr_reader :positions
   
   def initialize
-    @board = {1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5,
+    @positions = {1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5,
               6 => 6, 7 => 7, 8 => 8, 9 => 9}
   end
   
   def draw
     puts "-------------"
-    puts "| #{board[1]} | #{board[2]} | #{board[3]} |"
+    puts "| #{positions[1]} | #{positions[2]} | #{positions[3]} |"
     puts "-------------"
-    puts "| #{board[4]} | #{board[5]} | #{board[6]} |"
+    puts "| #{positions[4]} | #{positions[5]} | #{positions[6]} |"
     puts "-------------"
-    puts "| #{board[7]} | #{board[8]} | #{board[8]} |"
+    puts "| #{positions[7]} | #{positions[8]} | #{positions[8]} |"
     puts "-------------"
   end
   
   def update(player, position)
     x_or_o = {"user" => "X", "computer" => "o"}
-    self.board[board.key(position)] = x_or_o[player]
-    board
+    self.positions[positions.key(position)] = x_or_o[player]
+    positions
   end
   
   def available
     available_positions = []
-    board.each do |position, value|
+    positions.each do |position, value|
       available_positions << position unless ["X", "o"].include?(value)
     end
     available_positions
   end
+  
+  def available?(position)
+    positions.available.include?(position)
+  end
+  
 end
+
+class Player
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Person < Player
+  def ask_for_pick
+    puts "=> Where do you want to go next?"
+    user_pick = gets.chomp
+    until positions.available?(user_pick.to_i)
+      puts "=> Thats not a valid entry. Please enter a number for an available spot."
+      user_pick = gets.chomp
+    end
+    return user_pick
+  end # returns a board[value] like '1'
+end
+
+class Computer < Player
+end
+
+class TicTacToe
+  attr_accessor :player, :computer, :board
+  def initialize
+    @person = Person.new
+    @computer = Computer.new
+    @board = Board.new
+  end
+  
+  def run
+    until end_game
+      Board.draw
+      Person.ask_for_pick
+      Computer.pick
+      Board.check_for_winner
+    end
+  end
+end
+  
