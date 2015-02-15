178be7a8b0d931c561febef91ae58248be4e2da3
Tom Lee <tom_t_lee@yahoo.com>
2015-02-15 15:41:25 -0500

    tic-tac-toe with basic computer

--- tic-tac-toe/tic-tac-toe.rb
+++ tic-tac-toe/tic-tac-toe.rb
@@ -9,8 +9,7 @@ class Board
   ]
   
   def initialize
-    @positions = {1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5,
-              6 => 6, 7 => 7, 8 => 8, 9 => 9}
+    @positions = {1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9}
   end
   
   def draw
@@ -19,34 +18,30 @@ class Board
     puts "-------------"
     puts "| #{positions[4]} | #{positions[5]} | #{positions[6]} |"
     puts "-------------"
-    puts "| #{positions[7]} | #{positions[8]} | #{positions[8]} |"
+    puts "| #{positions[7]} | #{positions[8]} | #{positions[9]} |"
     puts "-------------"
   end
   
-  def update(player, position)
-    
-    self.positions[positions.key(position)] = x_or_o[player]
-    positions
+  def update(position, marker)
+    @positions[position.to_i] = marker
   end
   
   def available
-    available_positions = []
-    positions.each do |position, value|
-      available_positions << position unless ["X", "o"].include?(value)
-    end
-    available_positions
+    @positions.select { |key, value| value != "X" && value != "o" }.keys
   end
   
   def available?(position)
-    board.available.include?(position)
+    @positions.select { |key, value| value != "X" && value != "o" }.keys.include?(position)
+  end
+
+  def player_picks(marker)
+    @positions.select { |_, value| value == marker}.keys
   end
   
-  def check_for_winner(player)
-    person_positions = person.positions
-    computer_positions = computer.positions
+  def check_for_winner(marker)
     WINNING_COMBINATIONS.each do |combination|
-      if (combination & player_positions) == combination
-        return true
+      if (combination & player_picks(marker)) == combination
+        return marker
       end
     end
     nil
@@ -55,70 +50,107 @@ class Board
 end
 
 class Player
-  def initialize
-    puts "Player initialized."
+  attr_reader :marker
+  def initialize(name, marker)
+    @name = name
+    @marker = marker
   end
 
-  def positions
-    player_pieces = {"Person" => "X", "Computer" => "o"}
-    player_positions = []
-    board.each do |position, value|
-      player_positions << position if value == player_pieces[name]
-    end
-    player_positions
+  def picks
+    @board.player_picks(@marker)
   end
 end
-
-
-class Person < Player
-  def ask_for_pick
-    puts "=> Where do you want to go next?"
-    user_pick = gets.chomp.to_i
-    until Board.available?(user_pick)
-      puts "=> Thats not a valid entry. Please enter a number for an available spot."
-      user_pick = gets.chomp.to_i
-    end
-    return user_pick
-  end  
-end
-
-
-class Computer < Player
-  def pick
-    board.available.sample
-  end
-end
-
 
 class Game
-  attr_accessor :person, :computer, :board
+  attr_accessor :person, :computer, :board, :current_player, :turn_count
   
   def initialize
-    @person = Person.new
-    @computer = Computer.new
+    @person = Player.new("Bob", "X")
+    @computer = Player.new("Marvin", "o")
     @board = Board.new
+    @current_player = @person
+    @turn_count = 0
   end
   
-  def win_message(winner)
-    case winner
-      when "Person" then "You win!"
-      when "Computer" then "You lose."
-      when nil then "It's a draw."
+  def randomize_starting_player
+    @current_player = [@person, @computer].sample
+    puts @current_player == @person ? "You are first." : "Computer went first."
+  end
+
+  def current_player_turn
+    if @current_player == @person
+      begin
+        puts "=> Pick a position (1-9):"
+        pick = gets.chomp
+      end until @board.available?(pick.to_i)
+    elsif @current_player == @computer
+      pick = board.available.sample
     end
+    @board.update(pick, @current_player.marker)
+  end
+
+  def increment_turn_count
+    @turn_count += 1
+  end
+
+  def alternate_player
+    if @current_player == @person
+      @current_player = @computer
+    else
+      @current_player = @person
+    end
+  end
+
+  def clear_screen
+    system "cls"
+  end
+
+  def short_pause
+    sleep(1)
+  end
+
+  def welcome_message
+    "Welcome to tic-tac-toe!"
+  end
+
+  def win_message(marker)
+    if @turn_count >= 9
+      "It's a tie"
+    elsif marker == @person.marker
+      "You win!"
+    elsif marker == @computer.marker
+      "You lose."
+    end
+  end
+
+  def ask_play_again
+    puts "Would you like to play again? (y/n)."
+    play_again = gets.chomp.downcase
+    until ['y', 'n'].include?(play_again)
+      puts "Thats not a valid entry. Please type 'y' or 'n'."
+      play_again = gets.chomp.downcase
+    end
+    play_again
   end
   
   def run
-    turn_count = 0
+    clear_screen
+    puts welcome_message
+    randomize_starting_player
     begin
+      short_pause
+      clear_screen
       board.draw
-      person.ask_for_pick
-      turn_count += 1
-      break if board.check_for_winner(@person)
-      computer.pick
-      winner = board.check_for_winner(@computer)
-    end until winner
+      current_player_turn
+      increment_turn_count
+      winner = board.check_for_winner(@current_player.marker)
+      alternate_player
+    end until winner || @turn_count >= 9
+    system "cls"
+    board.draw
     puts win_message(winner)
+    puts "Thanks for playing!"
   end
 end
-  
+
 Game.new.run
